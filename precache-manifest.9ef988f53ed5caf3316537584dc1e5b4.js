98132462bd9b5ca91729859e6e81face1387fb5c
Tutak Rafał <coortes9@gmail.com>
2020-09-02 15:59:41 +0200

    Updates

---
+++ precache-manifest.9ef988f53ed5caf3316537584dc1e5b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22aeab66a05c24072bcad063f9c587ea",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "74b81c8fb1b00532803d",
+    "url": "/quiz/static/css/main.a72ff961.chunk.css"
+  },
+  {
+    "revision": "49f4dc013f47b3e8dd43",
+    "url": "/quiz/static/js/2.64dab5e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.64dab5e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b81c8fb1b00532803d",
+    "url": "/quiz/static/js/main.5449ea05.chunk.js"
+  },
+  {
+    "revision": "08dde15a5292136209df",
+    "url": "/quiz/static/js/runtime-main.9a5820ba.js"
+  },
+  {
+    "revision": "09760504475fff7fa06c5cbc340b6e8d",
+    "url": "/quiz/static/media/background.09760504.jpg"
+  }
+]);
